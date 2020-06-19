45ffead56fea73b979ff5589179a08fe4f9fb8d3
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-06-19 10:23:06 +0500

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseTracker from "./components/ExpenseTracker";
 
 function App() {
+  const [bool, setBool] = useState(true);
+
+  setBool(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ExpenseTracker />
       </header>
     </div>
   );
---
+++ src/components/ExpenseTracker.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const ExpenseTracker = (props: Props) => {
+  return <div>Hello World</div>;
+};
+
+export default ExpenseTracker;
