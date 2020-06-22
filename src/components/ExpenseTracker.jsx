4a1eabc5b3e19e14865df88192b32d5ae4b0f2cd
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-06-22 10:54:49 +0500

    Handled submission of form

--- src/components/ExpenseTracker.jsx
+++ src/components/ExpenseTracker.jsx
@@ -1,11 +1,17 @@
 import React, { useContext } from "react";
 import { AppCtx } from "../context/GlobalState";
 import { useForm } from "react-hook-form";
+import { v4 } from "uuid";
 
 const ExpenseTracker = (props) => {
   const { register, errors, handleSubmit, reset } = useForm();
-  const { addTransaction, deleteTransaction } = useContext(AppCtx);
-  const handleSubmitForm = (data) => console.log(data);
+  const { addTransaction } = useContext(AppCtx);
+  const handleSubmitForm = (data) => {
+    const amount = data.transactionType === "exp" ? -data.amount : +data.amount;
+    const transaction = { id: v4(), name: data.name, amount };
+    addTransaction(transaction);
+    reset();
+  };
 
   return (
     <div className="box">
@@ -20,6 +26,26 @@ const ExpenseTracker = (props) => {
               </select>
             </div>
           </div>
+        </div>
+        <div className="field">
+          <label className="label">Name</label>
+
+          <div className="control">
+            <input
+              className={`input ${errors.name ? "is-danger" : ""}`}
+              name="name"
+              placeholder="Name"
+              ref={register({ required: true })}
+              type="text"
+            />
+            <span className="icon is-small is-left">
+              <i className="fas fa-envelope"></i>
+            </span>
+            <span className="icon is-small is-right">
+              <i className="fas fa-exclamation-triangle"></i>
+            </span>
+          </div>
+          {errors.name && <p className="help is-danger">Name is required</p>}
         </div>
         <div className="field">
           <label className="label">Amount</label>
@@ -40,7 +66,9 @@ const ExpenseTracker = (props) => {
             </span>
           </div>
           {errors.amount && (
-            <p className="help is-danger">This amount is required </p>
+            <p className="help is-danger">
+              Amount is required and should be greater than 0
+            </p>
           )}
         </div>
         <input className="button is-primary" type="submit" />
